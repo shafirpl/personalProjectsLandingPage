f977b30e6281ad35a057f454b7160b51d3e0fe85
shafirpl <shafi.rpl@gmail.com>
2018-09-08 02:45:32 -0700

    minor fixes

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var express = require("express");
 var app     = express();
 
-var httpProxy = require('http-proxy');
-var apiProxy = httpProxy.createProxyServer();
-var serverOne = 'http://localhost:5000',
-    ServerTwo = 'http://localhost:8080';
+
 
 app.use(express.static("public"));
 app.set("view engine","ejs");
@@ -13,15 +10,7 @@ app.get("/",function(req,res){
   res.render("homepage");
 });
 
-app.all("/yelpcamp/*", function(req, res) {
-    console.log('redirecting to Server1');
-    apiProxy.web(req, res, {target: serverOne});
-});
 
-app.all("/newsportal/*", function(req, res) {
-    console.log('redirecting to Server2');
-    apiProxy.web(req, res, {target: ServerTwo});
-});
 
 app.listen(6000,function(){
     console.log("Server has started");
